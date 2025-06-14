b92a8eb6dc7d0f367ad899067ce967042405cba3
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-14 17:22:35 +0200

    aggiunto il supporto per visualizzare il nome dell'utente nella pagina utente e migliorata la gestione del login

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,47 +92,50 @@ const inputMisura1El = document.getElementById("inputMisura1");
 const inputMisura2El = document.getElementById("inputMisura2");
 const inputMisura3El = document.getElementById("inputMisura3");
 const inputMisura4El = document.getElementById("inputMisura4");
+const divTitleNameUserEl = document.getElementById("titleCard");
+console.log(divTitleNameUserEl);
+formLoginEL.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-if (formLoginEL) {
-  formLoginEL.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    let emailInput = inputEmailEL.value.toLowerCase();
-    let password = inputPasswordEL.value;
-    console.log(emailInput);
-    const utenteValid = iscritti.find((iscritto) => {
-      if (
-        emailInput === iscritto["email"] &&
-        password === iscritto["password"]
-      ) {
-        return true;
-      }
-    });
-    if (utenteValid) {
-      window.location.href = "./page_user.html";
-    } else {
-      const allerta = "Email o Password errati";
-      alert(allerta);
+  let emailInput = inputEmailEL.value.toLowerCase();
+  let password = inputPasswordEL.value;
+  console.log(emailInput);
+  const utenteValid = iscritti.find((iscritto) => {
+    if (emailInput === iscritto["email"] && password === iscritto["password"]) {
+      return true;
     }
-
-    const adminValid = admin.find((admin) => {
-      if (emailInput === admin["email"] && password === admin["password"]) {
-        return true;
-      }
-    });
-    if (adminValid) {
-      window.location.href = "./admin_page.html";
-    } else {
-      const allerta = "Email o Password errati";
-      alert(allerta);
-    }
-    console.log(adminValid);
   });
-}
 
-//pagina Utente
+  if (utenteValid) {
+    window.location.href = "./page_user.html";
 
-//
+    const viewFullNameEl = document.createElement("h2");
+    console.log(divTitleNameUserEl);
+    iscritti.forEach((iscritto) => {
+      if (emailInput === iscritto["email"]) {
+        viewFullNameEl.innerHTML = `${iscritto.name}`;
+        divTitleNameUserEl.appendChild(viewFullNameEl);
+        console.log(viewFullNameEl);
+      }
+    });
+  } else {
+    const allerta = "Email o Password errati";
+    alert(allerta);
+  }
+
+  const adminValid = admin.find((admin) => {
+    if (emailInput === admin["email"] && password === admin["password"]) {
+      return true;
+    }
+  });
+  if (adminValid) {
+    window.location.href = "./admin_page.html";
+  } else {
+    const allerta = "Email o Password errati";
+    alert(allerta);
+  }
+  console.log(adminValid);
+});
 
 btnLogutEL.addEventListener("click", function (e) {
   e.preventDefault();
@@ -141,39 +144,43 @@ btnLogutEL.addEventListener("click", function (e) {
 });
 
 // Azioni pagina admin
+if (formAdminMisureEL) {
+  iscritti.forEach((iscritto) => {
+    let nome = iscritto.name;
+    let surname = iscritto.surname;
 
-iscritti.forEach((iscritto) => {
-  let nome = iscritto.name;
-  let surname = iscritto.surname;
+    let fullName = `${nome} ${surname}`;
+    const optionEl = document.createElement("option");
+    optionEl.value = fullName;
+    optionEl.textContent = fullName;
+    selectUserEl.appendChild(optionEl);
+  });
+  const optionEl = document.querySelector("option");
 
-  let fullName = `${nome} ${surname}`;
+  formAdminMisureEL.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const misura1 = Number(inputMisura1El.value);
+    const misura2 = Number(inputMisura2El.value);
+    const misura3 = Number(inputMisura3El.value);
+    const misura4 = Number(inputMisura4El.value);
+    const selectedName = selectUserEl.value;
+    const trovato = iscritti.find(
+      ({ name, surname }) => `${name} ${surname}` === selectedName
+    );
 
-  const optionEl = document.createElement("option");
-  optionEl.value = fullName;
-  optionEl.textContent = fullName;
-  selectUserEl.appendChild(optionEl);
-});
-const optionEl = document.querySelector("option");
+    if (trovato) {
+      trovato.misuraUno = misura1;
+      trovato.misuraDue = misura2;
+      trovato.misuraTre = misura3;
+      trovato.misuraQuattro = misura4;
+      console.log("Dati salvati per:", trovato);
+    } else {
+      console.log("Nessun iscritto trovato.");
+    }
+    console.log(iscritti);
+  });
+}
 
-formAdminMisureEL.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const misura1 = Number(inputMisura1El.value);
-  const misura2 = Number(inputMisura2El.value);
-  const misura3 = Number(inputMisura3El.value);
-  const misura4 = Number(inputMisura4El.value);
-  const selectedName = selectUserEl.value;
-  const trovato = iscritti.find(
-    ({ name, surname }) => `${name} ${surname}` === selectedName
-  );
+//pagina Utente
 
-  if (trovato) {
-    trovato.misuraUno = misura1;
-    trovato.misuraDue = misura2;
-    trovato.misuraTre = misura3;
-    trovato.misuraQuattro = misura4;
-    console.log("Dati salvati per:", trovato);
-  } else {
-    console.log("Nessun iscritto trovato.");
-  }
-  console.log(iscritti);
-});
+//
