370d09ec13b4135a47136ca5ef3528c1a67bc98e
DavideCriscuolo <davide.alcatel@gmail.com>
2025-07-12 12:33:42 +0200

    fixata query per index

--- controller/gymController.js
+++ controller/gymController.js
@@ -1,19 +1,17 @@
 const connection = require("./../db/conncetion");
 
 const index = (req, res) => {
-  const sql = "SELECT * FROM gym;";
+  const sql = "SELECT * FROM iscritti;";
 
-  connection.query =
-    (sql,
-    (err, results) => {
-      if (err) {
-        return res.status(500).json({
-          err: err.message,
-        });
-      }
-      console.log(results);
-      res.json(results);
-    });
+  connection.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        err: err.message,
+      });
+    }
+    console.log(results);
+    res.json(results);
+  });
 };
 
 module.exports = {
